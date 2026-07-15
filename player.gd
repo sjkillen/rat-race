95954007d1a63e343f582def340998070c188fd0
luciacomputerscience <teamnamenoname@gmail.com>
2026-07-15 01:53:33 -0600

    Added retry screen. Redid map slightly. Added bubble shader and particle effects

--- player.gd
+++ player.gd
@@ -3,12 +3,15 @@ extends VehicleBody3D
 
 const MAX_STEER = 0.8  # 45 degrees limit on turn
 const ENGINE_POWER = 300
+const AUTOSTART_FORCE = 65
 var up_to_speed = false  # When true, allows player to die when below threshold
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	get_node("../UI/Retry").hide()
+	linear_velocity = global_basis * Vector3.MODEL_FRONT * AUTOSTART_FORCE
+
 	# Gets rid of mouse while playing
 	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
@@ -33,9 +36,9 @@ func _process(delta: float) -> void:
 	if fwd_mps <= 60 and up_to_speed == false:
 		# Disable the controls
 		engine_force = 0.0
-		steering = 0.0
+		# Uncomment to get rid of steering
+		#steering = 0.0
 		get_node("../UI/SpeedStatus").text = "Accelerating..."
-		apply_force(Vector3.MODEL_FRONT * ENGINE_POWER * 2)
 	
 	# Once up to speed, change the flag and allow player to accelerate at will
 	if fwd_mps >= 60 and up_to_speed == false:
@@ -47,4 +50,10 @@ func _process(delta: float) -> void:
 		destroy_car()
 	
 func destroy_car() -> void:
-	get_node("../UI/SpeedStatus").text = "Too Slow!"
+	get_node("../UI/Retry").show()
+	linear_velocity = (Vector3.ZERO)
+	#get_node("../UI/SpeedStatus").text = "Too Slow!"
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept") and get_node("../UI/Retry").visible:
+		get_tree().reload_current_scene()
