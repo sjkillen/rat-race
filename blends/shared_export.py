c5d3748874e09af78ac25e7b00bdea529d923ede
Spencer Killen <sjkillen@ualberta.ca>
2026-08-29 16:36:54 -0600

    Shark texturing and animation

--- blends/shared_export.py
+++ blends/shared_export.py
@@ -1,23 +1,15 @@
-"""
-Common .blend exporting functions to be used by .blend file text blocks (See README.md)
-"""
-
+from pathlib import Path
+import bpy
+from bpy.types import Object, Armature, Modifier, Mesh, Context, Collection
 from functools import reduce
 from operator import attrgetter, itemgetter, or_
-from pathlib import Path
-
-import bpy
-from bpy.types import Armature, Collection, Context, Mesh, Modifier, Object
-
 
 def project_path() -> Path:
     return Path(__file__).parent.parent.resolve()
 
-
 def clear_selection():
     for obj in bpy.context.selected_objects:
         obj.select_set(False)
-
 
 def select_collection(c: Collection):
     for obj in c.objects:
