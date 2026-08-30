27e80adc9077ec025100a1345a399010eb2e578c
Spencer Killen <sjkillen@ualberta.ca>
2026-08-30 11:43:53 -0600

    Add go offline func

--- autoloads/game.gd
+++ autoloads/game.gd
@@ -1,16 +1,21 @@
 extends Node
 
 func server() -> Server:
-	var room = room()
+	var room = serverclient()
 	if room == null or not room.multiplayer.is_server():
 		return null
 	return room as Server
 
 func client() -> Client:
-	var room = room()
+	var room = serverclient()
 	if room == null or room.multiplayer.is_server():
 		return null
 	return room as Client
 
-func room():
+func serverclient():
 	return get_node_or_null("ServerClientShared")
+
+func go_offline():
+	var room = serverclient()
+	if room != null:
+		remove_child(room)
